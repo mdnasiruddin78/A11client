a643ba96fc03fda5660ee1914c59bed8b3906480
mdnasiruddin78 <nasiruddinanik54@gmail.com>
2024-12-24 01:50:41 +0600

    motion animation add

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,27 @@
 import { Link } from "react-router-dom";
+import * as motion from "motion/react-client"
 
 
 const Card = ({ service }) => {
 
     const { _id, image, title, category, description, price } = service
+    
+    const box = {
+        width: 400,
+        height: 250,
+        backgroundColor: "#9911ff",
+        borderRadius: 10,
+    }
 
     return (
         <div className="card bg-[#082032] border-2 border-[#69779b]">
             <div className="px-5 pt-5">
-                <img
+                    <motion.img
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.8 }}
+                    style={box}
                     src={image}
-                    alt="image"
-                    className="rounded-xl h-[250px] w-[400px]" />
+                />
             </div>
             <div className="card-body">
                 <h2 className="card-title font-semibold text-white text-xl">Service Title: {title}</h2>
