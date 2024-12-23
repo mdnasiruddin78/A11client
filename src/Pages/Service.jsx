5345c078acc54810abb43c0b90ca16420bf61a0c
mdnasiruddin78 <nasiruddinanik54@gmail.com>
2024-12-23 18:05:28 +0600

    all service data get in servicer page

--- src/Pages/Service.jsx
+++ src/Pages/Service.jsx
@@ -1,6 +1,19 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 
 const Service = () => {
+
+    const [services,setServices] = useState([])
+    console.log(services)
+    useEffect(()=>{
+        const fetchAllService = async () => {
+          const {data} = await axios.get(`${import.meta.env.VITE_API_URL}/allService`)
+          setServices(data)
+        }
+        fetchAllService()
+      },[])
+
     return (
         <div>
             <h3 className="text-white">this is services page</h3>
